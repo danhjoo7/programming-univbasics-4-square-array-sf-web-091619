3ac22d4db2131074a1dcd3c797419fcf2f6142cb
Daniel Joo <danhjoo@gmail.com>
2019-09-10 22:29:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0
   
   while counter < array.length do
-    puts array[counter]**2 
+    new_value = array[counter]
+    puts new_value**2
     counter += 1
   end
 end
